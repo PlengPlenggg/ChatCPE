391246b15c3c54a0a38ba6263f71352bebe3a968
Pleng <plengtitaporn@gmail.com>
2026-05-10 23:21:15 +0700

    Delete test_api.sh

--- test_api.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-TOKEN='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjUsImV4cCI6MTcxMzE1OTA3MH0.test'
-echo  Testing /auth/users endpoint:
-curl -s http://localhost:8000/auth/users -H Authorization: Bearer  | jq . 2>/dev/null ; echo 'Failed to call API'
-echo 
-echo Testing /auth/profile endpoint:
-curl -s http://localhost:8000/auth/profile -H Authorization: Bearer  | jq . 2>/dev/null ; echo 'Failed to call API'
